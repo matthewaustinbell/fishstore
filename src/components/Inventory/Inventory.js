ed2de84f93cf35525b2701775750257791fa20d0
Matt Bell <matthewaustinbell@gmail.com>
2019-06-25 19:41:47 -0500

    wip

---
+++ src/components/Inventory/Inventory.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import fishData from '../../helpers/data/fishData';
+
+import './Inventory.scss';
+
+class Inventory extends React.Component {
+  state = {
+    fishes: [],
+  }
+
+  componentDidMount() {
+    fishData.getFishes()
+      .then(fishes => this.setState({ fishes }))
+      .catch(err => console.error('could not get fishes', err));
+  }
+
+  render() {
+    return (
+      <div className="Inventory">
+        <h1>Inventory</h1>
+      </div>
+    );
+  }
+}
+export default Inventory;
